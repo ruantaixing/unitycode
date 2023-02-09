2f562e3e447577d7aafe54ae6e5bbc97dbe6b688
ruantaixing <taixingruan2019@gmail.com>
2023-02-09 18:14:59 +0700

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public void GetInput(string guess) {
+        Debug.Log ("You Enter " + guess);
+    }
+}
